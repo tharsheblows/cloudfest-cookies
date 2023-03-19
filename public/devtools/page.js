02f6f1cafbea8a4f8279eee0a4fa9988c994d94a
Maximilian Andre <maximilian.andre@e-mundo.de>
2023-03-19 10:24:12 +0100

    Sh** is working yo

--- public/devtools/page.js
+++ public/devtools/page.js
@@ -3,6 +3,6 @@ console.log(chrome.devtools)
 chrome.devtools.panels.create(
   "Cloudfest Cookies",
   "hello.png",
-  "devtools/index.html",
+  "index.html",
   function (panel) { console.log(panel) }
 );
