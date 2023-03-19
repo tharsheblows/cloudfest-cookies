edb518b069bc9202451d2121d57f48d2a1d4a333
Stefan Euchehofer <stefan.euchenhofer@gmail.com>
2023-03-19 10:28:49 +0100

    Add tailwind

--- react/src/App.js
+++ react/src/App.js
@@ -1,16 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "@material/web/button/filled-button.js";
-import "@material/web/button/outlined-button.js";
-import "@material/web/checkbox/checkbox.js";
+import logo from './logo.svg';
+import './App.css';
+import Example from './components/Example';
 
 function App() {
   return (
     <div className="App">
-      <label>
-        Material 3<md-checkbox checked></md-checkbox>
-      </label>
-      <md-outlined-button label="Back"></md-outlined-button>
+      <Example />
     </div>
   );
 }
