24178c3d5ffbab4ff8e26ad10ff10ca1c972598d
Stefan Euchehofer <stefan.euchenhofer@gmail.com>
2023-03-19 14:08:44 +0100

    Add cookie subscription to react

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
