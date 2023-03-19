e062c2ce40b586714ee08690c2a526590e0c82ea
fabian.genes <fabian.genes@e-mundo.de>
2023-03-19 18:33:37 +0100

    Try to fix it

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,6 +1,37 @@
 import JSConfetti from 'js-confetti'
 
 
+console.log("TEST");
+
+  const badCookieAudio = new Audio(
+    'https://porchy.co.uk/wp-content/uploads/2023/03/mixkit-dog-barking-twice-1.wav'
+  );
+
+  // Listen for a bark.
+  chrome.runtime.onMessage.addListener(async function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    if (request.action === 'makeASound') {
+
+      const cookies = request.cookies;
+
+      cookies.forEach((c) => {
+        const hostname = window.location.hostname;
+        const cookie = new URL(c.url);
+
+        const firstParty = hostname === cookie.hostname;
+
+        if (!firstParty) {
+          badCookieAudio.play();
+        }
+      })
+      return { message: 'makeASound' };
+    }
+  });
+
+
 let readyStateCheckInterval = setInterval(function () {
   if (document.readyState === "complete") {
     clearInterval(readyStateCheckInterval);
@@ -59,38 +90,9 @@ function runScript() {
 
   })
 }
-console.log("Just add some content reactions")
 
-(function () {
- const badCookieAudio = new Audio(
-   'https://porchy.co.uk/wp-content/uploads/2023/03/mixkit-dog-barking-twice-1.wav'
- );
 
-  // Listen for a bark.
-   chrome.runtime.onMessage.addListener(async function (
-     request,
-     sender,
-     sendResponse
-   ) {
-     if (request.action === 'makeASound') {
 
-      const cookies = request.cookies;
-
-      cookies.forEach((c) => {
-        const hostname = window.location.hostname;
-        const cookie = new URL(c.url);
-
-        const firstParty = hostname === cookie.hostname;
-
-        if (!firstParty) {
-          badCookieAudio.play();
-        }
-      })
-       return { message: 'makeASound' };
-     }
-   });
-
-})();
 
   // Responsible for listening for a request to send the cookies to the browser.
 
