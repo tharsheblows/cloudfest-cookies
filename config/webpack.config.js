fc678f07d9ed5768f8546997a32d1066443e13d9
Maximilian Andre <maximilian.andre@e-mundo.de>
2023-03-19 14:46:54 +0100

    Removed browser sync and added cleanup job

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -18,35 +18,6 @@ const config = (env, argv) =>
       cookies: PATHS.src + '/storage/cookies.ts',
     },
     devtool: argv.mode === 'production' ? false : 'source-map',
-    plugins: [
-      new BrowserSyncPlugin(
-        {
-          host: 'localhost',
-          port: 3000,
-          files: [
-            {
-              match: ['**/*'],
-              fn: function (event, file) {
-                if (event === 'change') {
-                  const bs = require('browser-sync').get('bs-webpack-plugin');
-                  bs.reload();
-                }
-              },
-            },
-          ],
-          browser: 'google-chrome',
-          reloadDelay: 0,
-          reloadDebounce: 0,
-          reloadOnRestart: true,
-          notify: false,
-        },
-        {
-          reload: false,
-          injectCss: true,
-          name: 'bs-webpack-plugin',
-        }
-      ),
-    ],
   });
 
 module.exports = config;
