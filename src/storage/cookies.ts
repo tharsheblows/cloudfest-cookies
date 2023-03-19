75e4af0dc5591dc0acd972a5fa75e96e405dc2b8
Jan Vogt <jan@noreply.vogts.cloud>
2023-03-19 17:49:39 +0100

    [wip] Use chrome.storage for cookie store
    Terribly broken and messy

--- src/storage/cookies.ts
+++ src/storage/cookies.ts
@@ -1,42 +1,73 @@
-let _listeners: (() => void)[] = [];
-let _store: { cookies: Cookie[], url: URL | undefined } = { cookies: [], url: undefined }
+import cookie from 'cookie';
 
-type Cookie = {}
-type CookieStore = {
-  subscribe: (listener: () => void) => (() => void)
-  getSnapshot: () => { cookies: Cookie[], url: URL }
-  addCookie: (cookie: Cookie) => void
-  requestCookies: () => Promise<void>
+let _listeners: { [tabId: number]: (() => void)[] } = {};
+let _store: { [tabId: string]: { cookies: Cookie[], url: URL | undefined } } = {}
+
+type Cookie = {
+  [key: string]: string
+  url: string,
+  initiator: string | undefined
 }
-
-function emitChange() {
-  for (let listener of _listeners) {
-    listener();
+type CookieStore = {
+  addFromRequest: (tabId: number, request: Request) => void
+  getSyncStore: (tabId: number) => {
+    getSnapshot: () => { cookies: Cookie[], url: URL }
+    subscribe: (listener: () => void) => (() => void)
   }
 }
+type Request = {
+  headers: chrome.webRequest.HttpHeaders,
+  initiator: string | null, url: string
+}
+type Header = {
+  name: string;
+  value?: string;
+}
+
+const mkHeaderToCookie = (
+  url: string,
+  initiator: string | undefined) =>
+  (header: Header): Cookie | undefined => {
+    // console.log(`${header.name}: ${header.value}`)
+    if (!header.value ||
+      header.name.toLowerCase() !== 'set-cookie') { return }
+    const c = cookie.parse(header.value);
+    return {
+      ...c,
+      url,
+      initiator
+    }
+
+  }
+
+chrome.storage.onChanged.addListener((changes) => {
+  Object.keys(changes).forEach(tabId => { _store[tabId] = changes[tabId].newValue })
+  console.log("changed", _store)
+})
 
 export const cookies: CookieStore = {
-  addCookie(cookie: Cookie) {
-    _store = { ..._store, cookies: [..._store.cookies, cookie] }
-    emitChange()
+  async addFromRequest(tabId: number, { headers, initiator, url }) {
+    const newCookies = headers.map(mkHeaderToCookie(url, initiator))
+      .filter((x: Cookie | undefined) => x)
+    const _store = await chrome.storage.local.get(tabId + '')
+    if (!_store[tabId]) { _store[tabId] = { cookies: [], url: undefined } }
+    _store[tabId] = {
+      ..._store[tabId]
+      , cookies: [..._store[tabId].cookies, ...newCookies]
+    }
+    await chrome.storage.local.set(_store)
   },
-  subscribe(listener) {
-    _listeners = [..._listeners, listener];
-    return () => {
-      _listeners = _listeners.filter(x => x !== listener);
-    };
-  },
-  getSnapshot() {
-    // IMPORTANT: identity must change iff value has changed
-    return _store;
-  },
-  async requestCookies() {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    const response = await chrome.runtime.sendMessage({ tabId: tab.id, action: 'getCookies' });
-    // do something with response here, not outside the function
-    const cookies = response.cookies;
-    const url = tab.url ? new URL(tab.url) : undefined;
-    _store = { url, cookies }
-    emitChange();
-  }
+  getSyncStore: (tabId: number) => ({
+    subscribe(listener) {
+      const _listener = (changes: chrome.storage.StorageChange) => {
+        if (Object.keys(changes).includes(tabId + '')) { listener() }
+      }
+      chrome.storage.onChanged.addListener(_listener)
+      return () => chrome.storage.onChanged.removeListener(_listener)
+    },
+    getSnapshot() {
+      // IMPORTANT: identity must change iff value has changed
+      return _store[tabId];
+    },
+  })
 };
