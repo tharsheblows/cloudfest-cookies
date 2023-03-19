3d05eba306872e79d3329d844e49890ab9eaa3a8
bibAtWork <57631430+bibAtWork@users.noreply.github.com>
2023-03-19 13:02:20 +0100

    Pull changes from master (#17) (#18)
    * Detailed specification of the problem domain
    * Refined problem description
    * Added audience to project scope
    * Added optional project goal

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,6 +10,7 @@ const PATHS = require('./paths');
 const config = (env, argv) =>
   merge(common, {
     entry: {
+      app: '.react/src/index.js',
       contentScript: PATHS.src + '/contentScript.ts',
       deleteAllCookies: PATHS.src + '/deleteAllCookies.js',
       getCookies: PATHS.src + '/getCookies.js',
@@ -18,6 +19,35 @@ const config = (env, argv) =>
       cookies: PATHS.src + '/storage/cookies.ts',
     },
     devtool: argv.mode === 'production' ? false : 'source-map',
+    plugins: [
+      new BrowserSyncPlugin(
+        {
+          host: 'localhost',
+          port: 3000,
+          files: [
+            {
+              match: ['**/*'],
+              fn: function (event, file) {
+                if (event === 'change') {
+                  const bs = require('browser-sync').get('bs-webpack-plugin');
+                  bs.reload();
+                }
+              },
+            },
+          ],
+          browser: 'google-chrome',
+          reloadDelay: 0,
+          reloadDebounce: 0,
+          reloadOnRestart: true,
+          notify: false,
+        },
+        {
+          reload: false,
+          injectCss: true,
+          name: 'bs-webpack-plugin',
+        }
+      ),
+    ],
   });
 
 module.exports = config;
